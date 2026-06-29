2b84cff221b01cfaefd59a71e06b03a6ca00da79
yusufzaidan72-crypto <yusufzaidan72@gmail.com>
2026-06-29 10:54:31 +0000

    Remove debug signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,13 +31,7 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
-  }
+  
 
   buildTypes {
         release {
