11f3a95e1df5c3dedf30ba69c3989f1b0d46e053
yusufzaidan72-crypto <yusufzaidan72@gmail.com>
2026-06-29 11:06:59 +0000

    Fix KSP syntax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -125,6 +125,6 @@ dependencies {
   androidTestImplementation(libs.androidx.runner)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
-  "ksp"(libs.androidx.room.compiler)
-  "ksp"(libs.moshi.kotlin.codegen)
+  ksp(libs.androidx.room.compiler)
+  ksp(libs.moshi.kotlin.codegen)
 }
